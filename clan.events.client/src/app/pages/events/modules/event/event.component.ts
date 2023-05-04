06ca8a82b18df7165efcb887329ab79545f77f1b
mattiejas <mattiejas@outlook.com>
2023-05-04 22:10:25 +0200

    Remove memoize library

--- clan.events.client/src/app/pages/events/modules/event/event.component.ts
+++ clan.events.client/src/app/pages/events/modules/event/event.component.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
 import { EventResponse } from 'clan.events.common/events';
 import { Observable, filter, map, switchMap } from 'rxjs';
 import { notNullOrUndefined } from 'src/app/common/operators/not-undefined';
-import { Memoize } from 'typescript-memoize';
 import { Response } from 'clan.events.common/responses';
 import { EventsService } from '../../events.service';
+import { Memoized } from 'src/app/common/decorators/memoized.decorator';
 
 @Component({
   selector: 'app-event',
@@ -18,14 +18,14 @@ export class EventComponent {
     private readonly eventsService: EventsService
   ) {}
 
-  @Memoize() public get id$(): Observable<string> {
+  @Memoized public get id$(): Observable<string> {
     return this.route.paramMap.pipe(
       map((params) => params.get('id')),
       notNullOrUndefined()
     );
   }
 
-  @Memoize() public get event$(): Observable<Response<EventResponse>> {
+  @Memoized public get event$(): Observable<Response<EventResponse>> {
     return this.id$.pipe(
       switchMap((id) => this.eventsService.getEventById(id))
     );
