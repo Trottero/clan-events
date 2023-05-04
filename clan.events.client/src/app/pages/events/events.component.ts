8c3fa033291d0522e4dcfe65a384d095a4d2f931
mattiejas <mattiejas@outlook.com>
2023-05-04 16:16:22 +0200

    Use common model in both api and frontend

--- clan.events.client/src/app/pages/events/events.component.ts
+++ clan.events.client/src/app/pages/events/events.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { EventModel } from 'clan.events.common';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent {
-
+  public events: EventModel[] = [
+    {
+      id: '1',
+      name: 'Event 1',
+    },
+    {
+      id: '2',
+      name: 'Event 2',
+    },
+  ];
 }
