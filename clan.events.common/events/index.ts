51473530478fb0e7430f1fc291b3035fefddf3e6
mattiejas <mattiejas@outlook.com>
2023-05-05 18:10:04 +0200

    Remove forgotten files

--- clan.events.common/events/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './responses/event-list.response';
-export * from './responses/event.response';
-
-export * from './requests/get-events.request';
-
-export * from './enums/board-type.enum';
