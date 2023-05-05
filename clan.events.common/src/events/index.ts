5093a6799d6fe6702d3b21a5c4df1d9cc4e6fc4e
mattiejas <mattiejas@outlook.com>
2023-05-05 20:07:15 +0200

    watch changes for common

---
+++ clan.events.common/src/events/index.ts
@@ -0,0 +1,6 @@
+export * from './responses/event-list.response';
+export * from './responses/event.response';
+
+export * from './requests/get-events.request';
+
+export * from './enums/board-type.enum';
