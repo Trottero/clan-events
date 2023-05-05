327f6cfd1d77146d60b5bf70203efac89a86ed69
mattiejas <mattiejas@outlook.com>
2023-05-05 18:11:01 +0200

    Remove unused source map require

--- clan.events.api/src/main.ts
+++ clan.events.api/src/main.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('source-map-support').install();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './middleware/response.interceptor';
