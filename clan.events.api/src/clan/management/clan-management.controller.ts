3e07797f0c665eb9180736f378f28f304bfba62d
Trottero <wrlxs@hotmail.com>
2023-05-11 21:52:57 +0200

    Remove add member endpoint

--- clan.events.api/src/clan/management/clan-management.controller.ts
+++ clan.events.api/src/clan/management/clan-management.controller.ts
@@ -67,41 +67,6 @@ export class ClanManagementController {
     };
   }
 
-  @Post('members')
-  @HasRoleInClan(ClanRole.Owner, ClanRole.Admin)
-  async addMemberToClan(
-    @Param('clanName') clanName: string,
-    @User() user: UserClanContext,
-    @Body() body: AddClanMemberRequest,
-  ): Promise<ClanMemberResponse> {
-    if (!this.isAllowedToAssignRole(user.clanRole, body.clanRole)) {
-      throw new UnauthorizedException('Not allowed to assign role');
-    }
-
-    const clan = await this.clanService.getClanByName(clanName);
-    if (!clan) {
-      throw new NotFoundException('Clan not found');
-    }
-    try {
-      const membership = await this.clanMembershipService.addMemberToClan(
-        clan,
-        body.discordId,
-        body.clanRole as ClanRole,
-      );
-
-      return {
-        name: membership.user.name,
-        discordId: membership.user.discordId,
-        clanRole: membership.role,
-      };
-    } catch (ex: any) {
-      if (ex.message == 'User not found') {
-        throw new NotFoundException(ex.message);
-      }
-      throw ex;
-    }
-  }
-
   @Delete('members')
   @HasRoleInClan(ClanRole.Owner, ClanRole.Admin)
   async removeMemberToClan(
