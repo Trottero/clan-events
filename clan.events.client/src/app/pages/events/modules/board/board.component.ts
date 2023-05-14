5c6d519e0cc5421a189ba6861d77eec39f38cf5c
mattiejas <mattiejas@outlook.com>
2023-05-14 12:32:18 +0200

    Add spinner on top of board when tiles are loading

--- clan.events.client/src/app/pages/events/modules/board/board.component.ts
+++ clan.events.client/src/app/pages/events/modules/board/board.component.ts
@@ -11,12 +11,7 @@ import { SimpleBoardRenderer } from './simple-board-renderer';
 export class BoardComponent implements OnDestroy {
   private readonly boardService = inject(BoardService);
 
-  tiles$ = this.boardService.tiles$.pipe(
-    catchError(err => {
-      console.error(err.message);
-      return [];
-    })
-  );
+  tiles$ = this.boardService.tiles$;
 
   selectedTile$ = this.boardService.selectedTile$;
 
