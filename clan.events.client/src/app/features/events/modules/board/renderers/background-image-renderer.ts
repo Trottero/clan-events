071c9b6f5dab355592e59e4e9e728ffd1a0bd3a6
mattiejas <mattiejas@outlook.com>
2023-05-21 17:03:06 +0200

    Prevent background from flickering

--- clan.events.client/src/app/features/events/modules/board/renderers/background-image-renderer.ts
+++ clan.events.client/src/app/features/events/modules/board/renderers/background-image-renderer.ts
@@ -1,25 +1,20 @@
-import { map, of, share, shareReplay } from 'rxjs';
-import { Theme } from 'src/app/core/components/theming/theme';
+import { of } from 'rxjs';
 import { BoardRenderer } from './board-renderer';
 
 export class BackgroundImageRenderer extends BoardRenderer {
   override name: string = BackgroundImageRenderer.name;
-  canvasObjects$ = of([]);
+  override canvasObjects$ = of([]);
 
   backgroundImage$ = this.boardService.backgroundImageUri$;
 
-  private state = {
-    backgroundImage: '',
-  };
+  image: HTMLImageElement = new Image();
 
   constructor() {
     super();
 
     this.subscriptions.add(
       this.backgroundImage$.subscribe(backgroundImage => {
-        this.state = {
-          backgroundImage,
-        };
+        this.image.src = backgroundImage;
       })
     );
   }
@@ -28,15 +23,11 @@ export class BackgroundImageRenderer extends BoardRenderer {
     context: CanvasRenderingContext2D,
     cameraOffset: { x: number; y: number }
   ): void {
-    // center image around origin
-    const image = new Image();
-    image.src = this.state.backgroundImage;
-
-    const imageWidth = image.width;
-    const imageHeight = image.height;
+    const imageWidth = this.image.width;
+    const imageHeight = this.image.height;
 
     context.drawImage(
-      image,
+      this.image,
       -imageWidth / 2,
       -imageHeight / 2,
       imageWidth,
